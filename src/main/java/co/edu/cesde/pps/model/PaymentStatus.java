bf79348112570f9abf415f4428ac0a652f41f37a
wuraldine <wuraldine@outlook.com>
2026-05-12 12:00:00 -0500

    se hicieron anotaciones lombok en Cart, CartItem, Category, Order, OrderItem, OrderStatus, Payment, PaymentMethod, PaymentStatus

--- src/main/java/co/edu/cesde/pps/model/PaymentStatus.java
+++ src/main/java/co/edu/cesde/pps/model/PaymentStatus.java
@@ -1,5 +1,7 @@
 package co.edu.cesde.pps.model;
 
+import lombok.*;
+
 import java.util.Objects;
 
 /**
@@ -14,35 +16,19 @@ import java.util.Objects;
  * Relaciones (futuro - etapa02):
  * - 1:N con Payment (un estado puede aplicar a múltiples pagos)
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class PaymentStatus {
 
     private Long paymentStatusId;
     private String name;
 
-    // Constructor vacío (requerido para JPA futuro)
-    public PaymentStatus() {
-    }
-
     // Constructor con campos obligatorios
     public PaymentStatus(String name) {
-        this.name = name;
-    }
-
-    // Getters y Setters
-
-    public Long getPaymentStatusId() {
-        return paymentStatusId;
-    }
-
-    public void setPaymentStatusId(Long paymentStatusId) {
-        this.paymentStatusId = paymentStatusId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
@@ -62,12 +48,5 @@ public class PaymentStatus {
     }
 
     // toString sin navegación a objetos relacionados
-
-    @Override
-    public String toString() {
-        return "PaymentStatus{" +
-                "paymentStatusId=" + paymentStatusId +
-                ", name='" + name + '\'' +
-                '}';
-    }
+    
 }
