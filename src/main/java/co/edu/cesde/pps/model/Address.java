9013d30b0d4b2c13fcfcc258a4a69d541d608f7c
wuraldine <wuraldine@outlook.com>
2026-05-15 12:00:00 -0500

    creacion de etapa 7

--- src/main/java/co/edu/cesde/pps/model/Address.java
+++ src/main/java/co/edu/cesde/pps/model/Address.java
@@ -1,6 +1,8 @@
 package co.edu.cesde.pps.model;
 
 import co.edu.cesde.pps.enums.AddressType;
+import lombok.*;
+
 import java.util.Objects;
 
 /**
@@ -25,6 +27,14 @@ import java.util.Objects;
  * - N:1 con User (muchas direcciones pertenecen a un usuario)
  * - 1:N con Order (como shipping_address_id o billing_address_id)
  */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+
 public class Address {
 
     private Long addressId;
@@ -38,9 +48,6 @@ public class Address {
     private String postalCode;
     private Boolean isDefault;
 
-    // Constructor vacío (requerido para JPA futuro)
-    public Address() {
-    }
 
     // Constructor con campos obligatorios
     public Address(User user, AddressType type, String line1, String city,
@@ -69,87 +76,7 @@ public class Address {
         this.isDefault = isDefault != null ? isDefault : false;
     }
 
-    // Getters y Setters
 
-    public Long getAddressId() {
-        return addressId;
-    }
-
-    public void setAddressId(Long addressId) {
-        this.addressId = addressId;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public AddressType getType() {
-        return type;
-    }
-
-    public void setType(AddressType type) {
-        this.type = type;
-    }
-
-    public String getLine1() {
-        return line1;
-    }
-
-    public void setLine1(String line1) {
-        this.line1 = line1;
-    }
-
-    public String getLine2() {
-        return line2;
-    }
-
-    public void setLine2(String line2) {
-        this.line2 = line2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public Boolean getIsDefault() {
-        return isDefault;
-    }
-
-    public void setIsDefault(Boolean isDefault) {
-        this.isDefault = isDefault;
-    }
 
     // equals y hashCode basados en ID
 
@@ -168,19 +95,6 @@ public class Address {
 
     // toString sin navegación a objetos relacionados (solo IDs)
 
-    @Override
-    public String toString() {
-        return "Address{" +
-                "addressId=" + addressId +
-                ", userId=" + (user != null ? user.getUserId() : null) +
-                ", type=" + type +
-                ", line1='" + line1 + '\'' +
-                ", line2='" + line2 + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", country='" + country + '\'' +
-                ", postalCode='" + postalCode + '\'' +
-                ", isDefault=" + isDefault +
-                '}';
+
     }
-}
+
